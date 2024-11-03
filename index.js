203fa51af0d5e591df5d01f620511a804393a77e
sophjvlia <sophiejulianacrabtree@gmail.com>
2024-11-03 22:29:36 +0800

    Fixes

--- index.js
+++ index.js
@@ -105,10 +105,10 @@ app.get('/posts', async (req, res) => {
 });
 
 app.get('/posts/:id', async (req, res) => {
-  const blogId = parseInt(req.params.id);
+  const postId = parseInt(req.params.id);
 
   try {
-    const result = await pool.query('SELECT * FROM posts WHERE id = $1', [blogId]);
+    const result = await pool.query('SELECT * FROM posts WHERE id = $1', [postId]);
 
     if (result.rows.length === 0) {
       res.status(400).json({ error: 'Blog post not found' });
@@ -139,14 +139,14 @@ app.post('/posts', authenticateToken, async (req, res) => {
 });
 
 app.patch('/posts/:id', authenticateToken, async (req, res) => {
-  const blogId = parseInt(req.params.id);
+  const postId = parseInt(req.params.id);
   const { title, content } = req.body;
   const user_id = req.user.id;
 
   try {
     const result = await pool.query(
       'UPDATE posts SET title = $1, content = $2, user_id = $3 WHERE id = $4 RETURNING *',
-      [title, content, user_id, blogId]
+      [title, content, user_id, postId]
     );
 
     if (result.rows.length === 0) {
@@ -161,20 +161,20 @@ app.patch('/posts/:id', authenticateToken, async (req, res) => {
 });
 
 app.delete('/posts/:id', authenticateToken, async (req, res) => {
-  const blogId = parseInt(req.params.id);
+  const postId = parseInt(req.params.id);
   const user_id = req.user.id;
 
   try {
     const checkOwnership = await pool.query(
       'SELECT * FROM posts WHERE id = $1 AND user_id = $2',
-      [blogId, user_id]
+      [postId, user_id]
     );
 
     if (checkOwnership.rowCount === 0) {
       return res.status(404).json({ error: 'Blog post not found or not authorized to delete' });
     }
 
-    const result = await pool.query('DELETE FROM posts WHERE id = $1', [blogId]);
+    const result = await pool.query('DELETE FROM posts WHERE id = $1', [postId]);
 
     if (result.rowCount === 0) {
       return res.status(404).json({ error: 'Blog post not found' });
